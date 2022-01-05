3bd8e98f1ac99ea0e9cb32b639c2f82fd796eb32
spaun <ivansurmanov@yandex.ru>
2022-01-05 19:25:33 +0600

    Решили задачу 1.3

---
+++ Задача 1.3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_1._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string accountBalance = "0,37";
+            Console.WriteLine("Задача 7365");
+            string messageText = "Пора пополнить счёт!\nБаланс Вашего счета составляет "+ accountBalance + "руб.";
+            Console.WriteLine(messageText);
+
+        }
+    }
+}
