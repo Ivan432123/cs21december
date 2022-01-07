9572a70077e2ce9016a71efb674109433cf38204
Ivan432123 <ivansurmanov@yandex.ru>
2022-01-08 00:00:26 +0300

    Решили задачу 1.6

---
+++ Задача 1.6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_1._6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "Программирование это ";
+             
+            string b = Console.ReadLine();
+            Console.WriteLine(a + b + "!");
+        }
+    }
+}
