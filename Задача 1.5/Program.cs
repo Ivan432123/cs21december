2910acdbca08148f1f12a37e21c4dd1eea2235d0
Ivan432123 <ivansurmanov@yandex.ru>
2022-01-07 23:50:37 +0300

    Решили задачу 1.5

---
+++ Задача 1.5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_1._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = " нас не остановят!";
+            string b = Console.ReadLine();
+            Console.WriteLine(b + a);
+        }
+    }
+}
