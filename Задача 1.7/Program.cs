de70dfd54b223446d6853bda0661800dde671750
Ivan432123 <ivansurmanov@yandex.ru>
2022-01-08 00:13:16 +0300

    Решили задачу 1.7

---
+++ Задача 1.7/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_1._7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string A = Console.ReadLine();
+            string B = Console.ReadLine();
+            string C = B + " " + A;
+            Console.WriteLine(C);
+        }
+    }
+}
