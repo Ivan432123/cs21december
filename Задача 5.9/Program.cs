c3ca1911d38e1ea61cfb8b7c4bff8a92f24b6877
Ivan432123 <ivansurmanov@yandex.ru>
2022-01-09 20:09:35 +0300

    Процесс решения задачи 5.9

---
+++ Задача 5.9/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_5._9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double Xa, Ya, Xb, Yb, X, Y, r;
+            
+            Xa = Convert.ToDouble(Console.ReadLine());
+            Ya = Convert.ToDouble(Console.ReadLine());
+            Xb = Convert.ToDouble(Console.ReadLine());
+            Yb = Convert.ToDouble(Console.ReadLine());
+            X = Xb - Xa;
+            Y = Yb - Ya;
+            
+
+            r = Math.Atan(Y / X);
+            Console.WriteLine(r);
+        }
+    }
+}
