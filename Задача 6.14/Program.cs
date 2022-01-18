b150dbd553e301804d309bef0d94598ce7445e6e
Ivan432123 <ivansurmanov@yandex.ru>
2022-01-18 23:44:41 +0300

    Решение задачи 6.14

---
+++ Задача 6.14/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_6._14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+
+            while (a <= b)
+            {
+                Console.Write("{0};", a);
+                a++;
+            }
+            
+            
+                Console.WriteLine("Значение А должно быть не больше значения В");
+            
+            
+        }
+    }
+}
