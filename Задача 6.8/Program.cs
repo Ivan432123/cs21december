add9e95ff9d9610527a4be8fa487058f86b60602
Ivan432123 <ivansurmanov@yandex.ru>
2022-01-28 20:43:19 +0300

    реш 6.8

--- Задача 6.8/Program.cs
+++ Задача 6.8/Program.cs
@@ -12,28 +12,29 @@ namespace Задача_6._8
         {
             int x, y, z; 
             x = Convert.ToInt32(Console.ReadLine());
-
-            y = x + 30;
             
-            while (x <= y)
+                y = x + 30;
+            
+
+                while (x <= y)
                 
             {
                 
-                Console.Write("{0} ", x);
+                        Console.Write("{0} ", x);
                 x += 3;
                 
             }
-            Console.WriteLine("\n");
-            x = Convert.ToInt32(Console.ReadLine());
-            y = x - 40;
-            while (x >= y)
-            {
-                
-                Console.Write("{0} ", y);
-                
-                y += 4;
-            }
+                Console.WriteLine("\n");
+                x = 2;
+                y = x - 40;
+                while (x >= y)
+                {
+                   
+                        Console.Write("{0} ", y);
 
+                    y += 4;
+                }
+            
             
           
            
