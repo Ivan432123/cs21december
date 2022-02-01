030110d06ca7806879629518c2ea1ba2d32bf5e1
Ivan432123 <ivansurmanov@yandex.ru>
2022-02-02 01:23:52 +0300

    6.18

---
+++ Задача 6.18/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_6._18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+
+            c = a++;
+            c = a;
+            while (a <= b)
+            {
+                Console.WriteLine("{0}", c);
+                a++;
+                
+            }
+            c = a--;
+
+
+            
+                }
+        }
+    }
