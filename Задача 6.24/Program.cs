2e294dd48c845cff24a0a23698155123a2304019
Ivan432123 <ivansurmanov@yandex.ru>
2022-02-14 00:41:07 +0300

    6.23

---
+++ Задача 6.24/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_6._24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B;
+            A = int.Parse(Console.ReadLine());
+            B = int.Parse(Console.ReadLine());
+            while(A > B | A < B)
+            {
+                Console.WriteLine("{0}___{1}", A, B);
+                A++;
+                B++;
+            }
+        }
+    }
+}
