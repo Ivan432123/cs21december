362a1545d2841b6733195754adc532aa4a6a97ee
Ivan432123 <ivansurmanov@yandex.ru>
2022-02-21 22:22:38 +0300

    Решение 7.7

---
+++ Задача 7.7/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_7._7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int w, h, b, c, m;
+            Console.Write("Введите символ: ");
+            string a = Console.ReadLine();
+
+            Console.Write("Введите ширину прямойгольника: ");
+            w = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите высоту прямоугольника: ");
+            h = int.Parse(Console.ReadLine());
+
+            b = 0;
+            c = 0;
+            m = 0;
+            w--;
+            while(w > b)
+            {
+                Console.Write(" цикл 1 " + a, w);
+                b++;
+               
+            }
+            while(h > c)
+            {
+                Console.WriteLine(" цикл 2 " + a, h);
+                c++;
+
+                while (h > m)
+                {
+                    Console.Write(" цикл 3 " + a, m);
+                    m++;
+                }
+            }
+           
+
+        }
+    }
+}
