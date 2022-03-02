130134e93a1b7897d8e4aee9f03d14d878a087e8
spaun <ivansurmanov@yandex.ru>
2022-03-02 20:07:24 +0600

    Решение 8.2

---
+++ Задача 8.1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_8._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String data = Console.ReadLine();
+            if (String.IsNullOrWhiteSpace(data))
+            {
+                Console.WriteLine("Исходная строка пуста");
+                return;
+            }
+
+           
+
+            String[] arr = data.Split(' ');
+            int a = Int32.Parse(Console.ReadLine());
+
+            if (a > arr.Length || a == 3)
+            {
+
+                Console.WriteLine("Число А должно быть в интервале " + arr.Length);
+                return;
+            }
+
+            if (a < arr.Length || a < 0)
+            {
+
+                Console.WriteLine("Число А должно быть в интервале " + arr.Length);
+                return;
+            }
+
+            Console.WriteLine("В массиве длиной " + arr.Length);
+            Console.WriteLine("на индексе " + a);
+            Console.WriteLine("находится элемент со значением " + arr[a]);
+
+            
+        }
+    }
+}
---
+++ Задача 8.2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_8._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String data = Console.ReadLine();
+            String[] arr = data.Split(' ');
+            int a = 7;
+            
+            if()
+
+
+        }
+    }
+}
