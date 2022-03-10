2967ea0fabdfc1459a7fb0271dd61aa83c11b01e
Ivan432123 <ivansurmanov@yandex.ru>
2022-03-10 12:38:58 +0300

    Решил 8.2

--- Задача 8.2/Program.cs
+++ Задача 8.2/Program.cs
@@ -13,17 +13,18 @@ namespace Задача_8._2
             String data = Console.ReadLine();
 
 
-
+            // Проверка массива на пустой ввод от пользователя
             if (String.IsNullOrEmpty(data))
             {
                 Console.WriteLine("Исходная строка пуста");
                 return;
             }
 
-
+            // Находим в необработанных данных data элементы разделённые ","
             String[] arr = data.Split(',');
 
-            if (arr.Length < 9)
+            // Условия проверки длины массива заданные в if() и вывод на консоль
+            if (arr.Length >= 6)
             {
 
                 Console.WriteLine("Значение элемента на индексе 0 это " + arr[0]);
@@ -33,13 +34,37 @@ namespace Задача_8._2
                 return;
              }
 
-            if (arr.Length > 4 || arr.Length > 2 || arr.Length > 1)
+            if (arr.Length < 6 && arr.Length > 3)
             {
+                Console.WriteLine("Значение элемента на индексе 0 это " + arr[0]);
+                Console.WriteLine("Значение элемента на индексе 2 это " + arr[2]);
+                Console.WriteLine("Значение элемента на индексе 3 это " + arr[3]);
+                Console.WriteLine("Нет элемента на индексе 5");
+                return;
+            }
+
+            if (arr.Length <= 3 && arr.Length > 2)
+            {
+                Console.WriteLine("Значение элемента на индексе 0 это " + arr[0]);
+                Console.WriteLine("Значение элемента на индексе 2 это " + arr[2]);
+                Console.WriteLine("Нет элемента на индексе 3");
+                Console.WriteLine("Нет элемента на индексе 5");
+                return;
+            }
+
+            if (arr.Length <= 2)
+            {
+                Console.WriteLine("Значение элемента на индексе 0 это " + arr[0]);
                 Console.WriteLine("Нет элемента на индексе 2");
                 Console.WriteLine("Нет элемента на индексе 3");
                 Console.WriteLine("Нет элемента на индексе 5");
-
+                return;
             }
+           
+
+            
+
+            
         }
             
         }
