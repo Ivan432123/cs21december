9c14d181cfc0354ceefdbef3dc9c0ae63ebce956
Ivan432123 <ivansurmanov@yandex.ru>
2022-03-15 23:29:44 +0300

    8.9

---
+++ Задача 8.10/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Задача_8._10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String data = Console.ReadLine();
+
+            
+            if (String.IsNullOrWhiteSpace(data))
+            {
+                Console.WriteLine("Нет данных");
+                return;
+            }
+            String[] arr = data.Split(' ');
+            int i = 0;
+           
+            while (i < arr.Length)
+            {
+                Console.Write("{0} ", arr[i]);
+                i++;
+             
+            }
+
+        }
+    }
+}
--- Задача 8.9/Program.cs
+++ Задача 8.9/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Задача_8._9
 {
@@ -14,6 +11,44 @@ namespace Задача_8._9
             String[] arr = data.Split(' ');
             int B = int.Parse(Console.ReadLine());
             int E = int.Parse(Console.ReadLine());
+
+            if (B >= arr.Length || B < 0)
+            {
+                Console.WriteLine("Число В должно быть в интервале {0}", arr.Length);
+                return;
+            }
+            if (E >= arr.Length || E < 0)
+            {
+                Console.WriteLine("Число E должно быть в интервале {0}", arr.Length);
+                return;
+            }
+            while (B == E)
+            {
+                Console.Write("{0} ", arr[E]);
+                return;
+            }
+
+            while (B < arr.Length-1 && arr.Length <= 5)
+            {
+                Console.Write("{0} ", arr[B]);
+                B++;
+
+            }
+
+            while (B < E && B < arr.Length && arr.Length > 5)
+            {
+                Console.Write("{0} ", arr[B]);
+                B++;
+
+            }
+
+            while (B >= E && E <= arr.Length && arr.Length > 5)
+            {
+                Console.Write("{0} ", arr[E]);
+                E++;
+
+            }
+
 
         }
     }
