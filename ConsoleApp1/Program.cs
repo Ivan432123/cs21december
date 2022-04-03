2d6d376f7e48ea408bbcceebebab212abdd42005
Ivan432123 <ivansurmanov@yandex.ru>
2022-04-03 15:41:38 +0300

    min max

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,19 +10,30 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            int[][] numbers = new int[3][];
-            numbers[0] = new int[] { 1, 2 };
-            numbers[1] = new int[] { 1, 2, 3 };
-            numbers[2] = new int[] { 1, 2, 3, 4, 5 };
-            for (int i = 0; i < numbers.Length; i++)
+            int i, n = 3;
+            double[] a = new double[n];
+            for (i = 0; i < n; i++)
             {
-                
-                for (int j = 0; j < numbers[i].Length; j++)
-                {
-                    Console.Write($"{numbers[i][j]} \t");
-                }
-                Console.WriteLine();
+                Console.Write("Введите элемент массива: ");
+                a[i] = Convert.ToDouble(Console.ReadLine());
             }
+
+            double min = a[0];
+            for (i = 1; i < n; i++)
+            {
+                if (min > a[i])
+                    min = a[i];
+            }
+            Console.WriteLine("min = {0}", min);
+
+            double max = a[0];
+            for (i = 1; i < n; i++)
+            {
+                if (a[i] > max)
+                    max = a[i];
+            }
+            Console.WriteLine("max = {0}", max);
+
         }
 
 
